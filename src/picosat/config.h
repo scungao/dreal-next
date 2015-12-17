647752610fa55c97f6fa3871f17f60d53fe5a98d
Soonho Kong <soonhok@cs.cmu.edu>
2015-12-17 02:09:37 -0500

    feat(src/picosat): add version 960
    http://fmv.jku.at/picosat/

---
+++ src/picosat/config.h
@@ -0,0 +1,1 @@
+#define PICOSAT_VERSION "960"
---
+++ src/picosat/version.c
@@ -0,0 +1,13 @@
+#include "picosat/config.h"
+
+const char *
+picosat_version (void)
+{
+  return PICOSAT_VERSION;
+}
+
+const char *
+picosat_config (void)
+{
+  return "configured with dReal";
+}
